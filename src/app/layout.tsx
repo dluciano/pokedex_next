96b2e6fbf9b67fda7805e1edb962cc484f63b1ab
Dawlin Peña Luciano <dawmusic19@gmail.com>
2023-01-18 14:21:11 +0000

    clear default template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,16 @@
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body className="lg:mx-auto lg:max-w-5xl lg:container min-h-screen border">
+        {children}
+      </body>
     </html>
-  )
+  );
 }
