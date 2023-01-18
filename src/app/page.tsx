f1658330ff62005cab5c7ac497b6da8e2eb3ce24
Dawlin Peña Luciano <dawmusic19@gmail.com>
2023-01-18 23:51:51 +0000

    add home page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <main>Hello world</main>;
+import pokeApiService from "@/api/pokeapi/PokeApiService";
+import { H1 } from "@components/typography";
+
+export default async function Home() {
+  return (
+    <div className={`px-[60px]`}>
+      <header>
+        <H1 className={`pl-[6px] py-[54px]`}>POK&Eacute;DEX</H1>
+      </header>
+    </div>
+  );
 }
