4029c89d455cac787d8989e58f3d7719034de2f9
Dawlin Peña Luciano <dawmusic19@gmail.com>
2023-01-18 18:22:19 +0000

    extend tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        pokeCream: "#F2F1F1",
+        pokeGray1: "#D8D8D8",
+        pokeGray2: "#606C78",
+        pokeGray3: "#1F2532",
+        pokeBlue: "#4A90E2",
+        pokePurple: "#BE93E7",
+        pokeOrange: "#E85B0E",
+        pokeGreen: "#A0DA5E"
+      },
+      fontSize:{
+        "1_3": "1.2rem",
+        "3_2": "3.2rem"
+      }
+    },
   },
   plugins: [],
 }
