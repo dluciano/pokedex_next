0c24f852d90771c34c40b3151196ed2c029f8c06
Dawlin Peña Luciano <dawmusic19@gmail.com>
2023-01-19 02:58:12 +0000

    add `raw.githubusercontent.com` to trusted images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
