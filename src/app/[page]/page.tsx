a20be475942b5681d0af9e9c12b66d75a33fbc04
Dawlin Peña Luciano <dawmusic19@gmail.com>
2023-01-19 12:29:22 +0000

    use `PokemonGrid` component

--- src/app/[page]/page.tsx
+++ src/app/[page]/page.tsx
@@ -1,8 +1,5 @@
 import pokeApiService from "@/api/pokeapi/PokeApiService";
-import { H1 } from "@components/typography";
-import { PokemonCard } from "@components";
-import { v4 as uuidv4 } from "uuid";
-import Link from "next/link";
+import { PokemonGrid } from "@components";
 
 const getPokemonPage = async (page: number) => {
   const pokemons = await pokeApiService.fetchNext(page, 12);
@@ -13,36 +10,14 @@ const getPokemonPage = async (page: number) => {
   };
 };
 
-export default async function Pokemons({
+export default async function PokemonsByPage({
   params,
 }: {
   params: { page: string };
 }) {
-  const page = parseInt(params.page);
-  const nextPage = `\\${page + 1}`;
+  const pageNumber = parseInt(params.page);
   const {
     data: { pokemons },
-  } = await getPokemonPage(page);
-  return (
-    <div className={`pl-[60px]`}>
-      <header>
-        <H1 className={`pl-[6px] py-[54px]`}>POK&Eacute;DEX</H1>
-      </header>
-      <main>
-        {pokemons.map((pokemon) => (
-          <PokemonCard {...pokemon} key={uuidv4()} />
-        ))}
-      </main>
-      <footer className="text-center w-full">
-        {pokemons.length === 12 && (
-          <Link
-            href={nextPage}
-            className="w-[216px] h-[44px] bg-pokeBlue font-montserrat font-bold rounded-md mb-[54px] inline-block p-2"
-          >
-            LOAD MORE
-          </Link>
-        )}
-      </footer>
-    </div>
-  );
+  } = await getPokemonPage(pageNumber);
+  return <PokemonGrid page={pageNumber} pokemons={pokemons} />;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 import pokeApiService from "@/api/pokeapi/PokeApiService";
-import { H1 } from "@components/typography";
-import { PokemonCard } from "@components";
-import { v4 as uuidv4 } from "uuid";
-import Link from "next/link";
+import { PokemonGrid } from "@components";
 
-const getPokemonPage = async () => {
-  const pokemons = await pokeApiService.fetchNext(0, 12);
+const getPokemonPage = async (page: number) => {
+  const pokemons = await pokeApiService.fetchNext(page, 12);
   return {
     data: {
       pokemons,
@@ -13,28 +10,10 @@ const getPokemonPage = async () => {
   };
 };
 
-export default async function Home() {
+export default async function Pokemons() {
+  const pageNumber = 0;
   const {
     data: { pokemons },
-  } = await getPokemonPage();
-  return (
-    <div className={`pl-[60px]`}>
-      <header>
-        <H1 className={`pl-[6px] py-[54px]`}>POK&Eacute;DEX</H1>
-      </header>
-      <main>
-        {pokemons.map((pokemon) => (
-          <PokemonCard {...pokemon} key={uuidv4()} />
-        ))}
-      </main>
-      <footer className="text-center w-full">
-        <Link
-          href="/1"
-          className="w-[216px] h-[44px] bg-pokeBlue font-montserrat font-bold rounded-md mb-[54px] inline-block p-2"
-        >
-          LOAD MORE
-        </Link>
-      </footer>
-    </div>
-  );
+  } = await getPokemonPage(pageNumber);
+  return <PokemonGrid page={pageNumber} pokemons={pokemons} />;
 }
