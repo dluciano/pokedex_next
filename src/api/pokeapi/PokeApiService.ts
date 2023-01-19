fd00b0a1124771955c59d1830de7de200b9df61f
Dawlin Peña Luciano <dawmusic19@gmail.com>
2023-01-19 02:57:23 +0000

    transform pokeapi graphql types to ts types

--- src/api/pokeapi/PokeApiService.ts
+++ src/api/pokeapi/PokeApiService.ts
@@ -1,13 +1,54 @@
 import { GraphQLClient } from "graphql-request";
 import { getSdk } from "./generated/graphql";
 
+type PokemonListDetail = {
+  name: string;
+  id: number;
+  types: string[];
+  image: string | undefined;
+};
+
 class PokeApiService {
   private readonly sdk: ReturnType<typeof getSdk>;
   constructor() {
     this.sdk = getSdk(new GraphQLClient(process.env.POKEAPI_GRAPHQL_URL));
   }
-  fetchNext = async () => {
-    return await this.sdk.getListOfPokemonsByPage({offset: 0, limit: 12});
+  fetchNext: () => Promise<PokemonListDetail[]> = async () => {
+    const response = await this.sdk.getListOfPokemonsByPage({
+      offset: 0,
+      limit: 12,
+    });
+
+    const pokemons: PokemonListDetail[] = response.pokemon_v2_pokemon.map(
+      (pokemon) => {
+        let image: string | undefined = "";
+        if (pokemon.pokemon_v2_pokemonsprites.length > 0) {
+          const spriteObj = JSON.parse(
+            pokemon.pokemon_v2_pokemonsprites[0].sprites
+          );
+          if (
+            "other" in spriteObj &&
+            "official-artwork" in spriteObj.other &&
+            "front_default" in spriteObj.other["official-artwork"]
+          )
+            image = spriteObj.other["official-artwork"].front_default;
+        }
+
+        return {
+          name: pokemon.name,
+          id: pokemon.id,
+          image,
+          types: pokemon.pokemon_v2_pokemontypes
+            .filter((type) => type.pokemon_v2_type && type.pokemon_v2_type.name)
+            .map((type) => {
+              if (!type.pokemon_v2_type || !type.pokemon_v2_type.name)
+                throw new Error("pokemon_v2_type or type.name is null");
+              return type.pokemon_v2_type.name.toLocaleLowerCase();
+            }),
+        };
+      }
+    );
+    return pokemons;
   };
 }
 
