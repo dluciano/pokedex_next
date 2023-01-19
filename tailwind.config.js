94f44219c68559db0f739470441d9a6be30cda04
Dawlin Peña Luciano <dawmusic19@gmail.com>
2023-01-19 02:09:18 +0000

    add missing color to tailwind.config + format file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,26 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors:{
+      colors: {
         pokeCream: "#F2F1F1",
         pokeGray1: "#D8D8D8",
         pokeGray2: "#606C78",
         pokeGray3: "#1F2532",
+        pokeGray4: "B0B9C4",
         pokeBlue: "#4A90E2",
         pokePurple: "#BE93E7",
         pokeOrange: "#E85B0E",
-        pokeGreen: "#A0DA5E"
+        pokeGreen: "#A0DA5E",
       },
-      fontSize:{
+      fontSize: {
         "1_3": "1.2rem",
-        "3_2": "3.2rem"
+        "3_2": "3.2rem",
       },
-      fontFamily:{
+      fontFamily: {
         // @see: https://beta.nextjs.org/docs/optimizing/fonts#local-fonts
         // @see: https://tailwindcss.com/docs/font-family#customizing-your-theme
         /*
@@ -29,10 +28,10 @@ module.exports = {
             If you’re using a font that contains an invalid identifier, 
             wrap it in quotes or escape the invalid characters.
         */
-        montserrat: ['var(--font-montserrat)', ...defaultTheme.fontFamily.sans],
-        din: ['var(--font-din)', ...defaultTheme.fontFamily.sans]
-      }
+        montserrat: ["var(--font-montserrat)", ...defaultTheme.fontFamily.sans],
+        din: ["var(--font-din)", ...defaultTheme.fontFamily.sans],
+      },
     },
   },
   plugins: [],
-}
+};
