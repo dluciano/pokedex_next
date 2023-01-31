21dde54030a3b2464a29c472fcddc8cc66b7a10b
Dawlin Peña Luciano <dawmusic19@gmail.com>
2023-01-31 20:16:05 +0000

    add adsense

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,10 +1,15 @@
 export default function Head() {
   return (
     <>
-      <title>Create Next App</title>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4728445304550491"
+        crossOrigin="anonymous"
+      ></script>
+      <title>Pokedex - Pokemon Wikipedia</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Pokedex" />
+      <meta name="description" content="Pokedex - pokemon wikipedia and reference" />
       <link rel="icon" href="/favicon.ico" />
     </>
-  )
+  );
 }
