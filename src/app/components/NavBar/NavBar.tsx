b8dca9ed561bfb5de48565cfe0f1222993a7724f
Dawlin Peña Luciano <dawmusic19@gmail.com>
2023-02-08 20:51:58 +0000

    move privacy to `app` folder

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -27,7 +27,7 @@ export const NavBar: FC<HTMLAttributes<HTMLElement>> = ({ className }) => (
     </Link>
     <Link
       className={`font-montserrat text-blue-400 inline-block h-full align-top underline-offset-1 float-right ${styles.navbar_privacy}`}
-      href="/privacy.html"
+      href="/privacy"
     >
       Privacy
     </Link>
